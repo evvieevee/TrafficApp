d99a38706f7086277a8395cd54c1741a6d0e295f
evvieevee <eve.puurtinen00@gmail.com>
2022-11-16 17:18:16 +0200

    adds functionality which shows trains and the times from the stop

--- main.js
+++ main.js
@@ -5,20 +5,44 @@ async function getData(url) {
     return await fetch(url).then(x => x.json())
 }
 
+function findTimeStamps(timeStamps, stationShortCode) {
+    const times = timeStamps.filter(x => x.stationShortCode === stationShortCode);
+    console.log(times);
+    return times;
+}
+
+function getTrainTime(listaJunia, stationShortCode) {
+    const listElement = document.getElementById('list');
+    listaJunia.forEach(juna => {
+        const li = document.createElement("li");
+        li.textContent = juna.trainNumber;
+        const times = findTimeStamps(juna.timeTableRows, stationShortCode);
+        const divArrival = document.createElement("div");
+        divArrival.textContent = "Arrival: " + times.find(x => x.type === "ARRIVAL").scheduledTime;
+        li.appendChild(divArrival);
+        const divDeparture = document.createElement("div");
+        divDeparture.textContent = "Departure: " + times.find(x => x.type === "DEPARTURE").scheduledTime;
+        li.appendChild(divDeparture);
+        listElement.appendChild(li);
+    });
+}
+
 async function onClickEvent(){
-    console.log("hei, painoit minua")
    const text = document.getElementById("inputField").value
-   console.log(text)
    const found = stations.find(x => x.stationName === text);
-   console.log(found)
    const dateNow = new Date();
-   const trains = await getData(`https://rata.digitraffic.fi/api/v1/routesets/station/${found.stationShortCode}/${dateNow.getFullYear()}-${dateNow.getMonth()}-${dateNow.getDate()}`);
+   const trains = await getData(`https://rata.digitraffic.fi/api/v1/live-trains/station/${found.stationShortCode}`);
+
+   //const trains = await getData(`https://rata.digitraffic.fi/api/v1/routesets/station/${found.stationShortCode}/${dateNow.getFullYear()}-${dateNow.getMonth()}-${dateNow.getDate()}`);
    console.log(trains)
+   getTrainTime(trains, found.stationShortCode)
+
 }
 
 
 (async function init() {
     stations = await getData('https://rata.digitraffic.fi/api/v1/metadata/stations');
     stationsNames = stations.map(x => x.stationName)
-    console.log(stationsNames)
+    console.log(stations)
 })()
+
