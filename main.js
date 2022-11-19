4a4c4cebac764069548d1528fbbda6f47202286c
evvieevee <eve.puurtinen00@gmail.com>
2022-11-19 15:53:49 +0200

    Cleans code

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Tässä määritellään variablet(muuttujat) nimeltä asemat ja aseman nimet.
 let stations = [];
 let stationsNames = [];
-//Tässä tapahtuu AJAX kutsu, jossa pyydetään dataa urlin takaa päivittämättä sivua välissä. 
+//Tässä tapahtuu AJAX kutsu, jossa pyydetään dataa urlin takaa päivittämättä sivua. 
 function createAjax(url) {
     return new Promise((resolve, reject) => {
         const xhttp = new XMLHttpRequest();
@@ -15,20 +15,18 @@ function createAjax(url) {
     })
 }
 
-
 async function getData(url) {
     //return await fetch(url).then(x => x.json())
-    // Tässä tarkistetaan mahdolliset errorit. S
-    return await createAjax(url).then(x => x).catch(error => console.error(error));
+    // Tässä tarkistetaan mahdolliset errorit. 
+    return await createAjax(url).catch(error => console.error(error));
 }
-
+// Tässä kohtaa etsitään aikatauluista haluttu aseman saapumis- ja lähtöajat.
 function findTimeStamps(timeStamps, stationShortCode) {
-    const times = timeStamps.filter(x => x.stationShortCode === stationShortCode);
-    console.log(times);
-    return times;
+    return timeStamps.filter(x => x.stationShortCode === stationShortCode);
 }
 // Tässä haetaan junien aikataululista. Listassa on saapumis- ja lähtöajat. 
 //  Lista tulostaa päivämäärän, sekä tunnit ja minuutit. Sekunnit on otettu pois.
+// ja lisätään käyttäjälle ne näkyviin.
 function getTrainTime(listaJunia, stationShortCode) {
     const listElement = document.getElementById('list');
     listElement.innerHTML = "";
@@ -52,20 +50,15 @@ async function onClickEvent(){
    const text = document.getElementById("inputField").value
    document.getElementById("asemanNimi").textContent = text;
    const found = stations.find(x => x.stationName.toLowerCase() === text.toLowerCase());
-   const dateNow = new Date();
    const trains = await getData(`https://rata.digitraffic.fi/api/v1/live-trains/station/${found.stationShortCode}`);
 
-   //const trains = await getData(`https://rata.digitraffic.fi/api/v1/routesets/station/${found.stationShortCode}/${dateNow.getFullYear()}-${dateNow.getMonth()}-${dateNow.getDate()}`);
-   console.log(trains)
    getTrainTime(trains, found.stationShortCode)
-
 }
 
-// Tässä haetaan junapysäkkien nimilista.
+// Tässä haetaan junapysäkkien nimilista heti kun käyttäjä siirtyy sivulle.
 (async function init() {
     stations = await getData('https://rata.digitraffic.fi/api/v1/metadata/stations');
     stationsNames = stations.map(x => x.stationName);
     autocomplete(document.getElementById("inputField"), stationsNames)
-    console.log(stationsNames)
 })()
 
